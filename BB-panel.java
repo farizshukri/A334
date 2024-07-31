9ba424c37d7ca232b47e6741765cea493aa8b624
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-07-31 18:20:30 +0800

    initial commit

---
+++ BB-panel.java
@@ -0,0 +1,10 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.util.ArrayList;
+import java.util.List;
