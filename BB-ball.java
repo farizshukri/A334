c9779252774efc83955566d209e6e000870d7fac
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-08-02 18:20:30 +0800

    initial commit

--- BB-ball.java
+++ BB-ball.java
@@ -2,4 +2,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+public class Ball {
+    private int x, y, xDirection, yDirection;
+    private final int diameter = 15;
 
+    
+}
