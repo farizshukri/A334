c7e1d98b8e5622fe17dc1f8fa674b7a7853db0af
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-08-03 18:20:30 +0800

    initial commit

--- BB-ball.java
+++ BB-ball.java
@@ -6,5 +6,12 @@ public class Ball {
     private int x, y, xDirection, yDirection;
     private final int diameter = 15;
 
-    
+    public Ball(int x, int y, int xDirection, int yDirection) {
+        this.x = x;
+        this.y = y;
+        this.xDirection = xDirection;
+        this.yDirection = yDirection;
+    }
+
+   
 }
