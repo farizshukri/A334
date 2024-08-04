eaeac84d344014aed2069a631e675ad7f7771b3e
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-08-04 18:20:30 +0800

    initial commit

--- BB-paddle.java
+++ BB-paddle.java
@@ -2,3 +2,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+
+    public Paddle(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
